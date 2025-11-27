904789ace7ca4f9ce718958e0be63086cadeb254
Repetitive <git@crii.xyz>
2025-11-27 14:35:23 +0100

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            svelte-language-server
+            typescript-language-server
+            bun
+          ];
+        };
+      });
+
+}
