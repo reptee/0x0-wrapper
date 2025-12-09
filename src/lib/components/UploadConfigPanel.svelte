111af41734c260c5a1f035d6f8fa277526034cfa
Repetitive <git@crii.xyz>
2025-12-09 10:36:48 +0100

    simplify upload config

--- src/lib/components/UploadConfigPanel.svelte
+++ src/lib/components/UploadConfigPanel.svelte
@@ -18,14 +18,14 @@
   }
 </script>
 
-<div id="settings-card">
-  <header id="title"><span class="icon">⚙️</span> settings</header>
+<div class="settings-card">
+  <header><span class="icon">⚙️</span> settings</header>
   <div>
-    <p>Expiry date (optional)</p>
+    <span>Expiry date (optional): </span>
     <input
       type="date"
       name="Expiry date"
-      id=""
+      id="expiry-date-selector"
       onchange={on_date_change}
       min={new Date(Date.now()).toISOString().split("T")[0]}
       max={new Date(Date.now() + max_age_days * 86_400_000)
@@ -38,13 +38,17 @@
     <input
       type="checkbox"
       name="Secret"
-      id="Secret"
+      id="is-secret-checkbox"
       bind:checked={config.secret}
     />
   </div>
   <div>
-    <p>Select provider</p>
-    <select name="0x0 provider" bind:value={config.provider}>
+    <span>Provider: </span>
+    <select
+      id="provider-selector"
+      name="0x0 provider"
+      bind:value={config.provider}
+    >
       {#each providers as provider, i}
         <option value={i}>{provider.url.hostname}</option>
       {/each}
@@ -53,7 +57,13 @@
 </div>
 
 <style lang="scss">
-  #settings-card {
+  header {
+    font-weight: 600;
+    align-items: center;
+    gap: 0.5rem;
+  }
+
+  .settings-card {
     background: #fff;
     border: 1px solid rgba(15, 23, 42, 0.06);
     border-radius: 1rem;
@@ -62,34 +72,29 @@
       transform 200ms ease;
 
     padding: 1.5rem;
-    display: flex;
-    flex-direction: column;
-    gap: 1rem;
     box-shadow: 0 8px 30px rgba(15, 23, 42, 0.06);
 
     &:hover {
       transform: translateY(-2px);
       box-shadow: 0 15px 45px rgba(15, 23, 42, 0.12);
     }
-
-    & label {
-      color: rgb(71, 85, 105);
-      font-size: 0.9rem;
-    }
-
-    & input,
-    & select {
-      border-radius: 0.5rem;
-      border: 1px solid #d6dae3;
-      padding: 0.65rem 0.75rem;
-      width: auto;
-    }
   }
 
-  #title {
-    font-weight: 600;
-    display: flex;
-    align-items: center;
-    gap: 0.5rem;
+  #provider-selector,
+  #expiry-date-selector,
+  #is-secret {
+    display: inline;
+    border-radius: 0.5rem;
+    border: 1px solid #d6dae3;
+    padding: 0.65rem 0.75rem;
+    width: auto;
+  }
+
+  input[type="checkbox"] {
+    transform: scale(1.5, 1.5);
+  }
+
+  .settings-card > div {
+    margin-top: 10pt;
   }
 </style>
