9cbf5cb04ac00fc30ba4154d0354b6f990858bc6
Repetitive <git@crii.xyz>
2026-01-17 17:49:14 +0100

    fix: popraw czytelność /credits

--- src/routes/credits/+page.svelte
+++ src/routes/credits/+page.svelte
@@ -1,33 +1,35 @@
-<h2>Ludzie</h2>
-<ul class="hearts">
-  <li>
-    <a href="https://git.0x0.st/mia">Mia</a> za stworzenie 0x0 i hostowanie go dla
-    wszystkich
-  </li>
-  <li>
-    Operatorzy
-    <a href="https://boop.icu">boop.icu</a> oraz
-    <a href="https://0.vern.cc">0.vern.cc</a>
-  </li>
-  <li>Zespół Svelte za framework</li>
-</ul>
+<div class="content">
+  <h2>Ludzie</h2>
+  <ul class="hearts">
+    <li>
+      <a href="https://git.0x0.st/mia">Mia</a> za stworzenie 0x0 i hostowanie go
+      dla wszystkich
+    </li>
+    <li>
+      Operatorzy
+      <a href="https://boop.icu">boop.icu</a> oraz
+      <a href="https://0.vern.cc">0.vern.cc</a>
+    </li>
+    <li>Zespół Svelte za framework</li>
+  </ul>
 
-<h2>Użyte narzędzia</h2>
-<ul class="tools">
-  <li>
-    <a href="https://wiki.nixos.org/wiki/Nix_(package_manager)">nix</a>
-    (menedżer pakietów), <a href="https://nixos.org/">NixOS</a> (system
-    operacyjny),
-    <a href="https://github.com/NixOS/nixpkgs/issues">nixpkgs</a> (kolekcja pakietów)
-  </li>
-  <li>
-    <a href="https://svelte.dev/">Svelte</a> oraz
-    <a href="https://svelte.dev/docs/kit/introduction">SvelteKit</a>
-  </li>
-  <li>hosting plików <a href="https://git.0x0.st/mia/0x0">0x0</a></li>
-  <li><a href="https://www.typescriptlang.org/">TypeScript</a></li>
-  <li><a href="https://sass-lang.com/documentation/syntax/">SCSS</a></li>
-</ul>
+  <h2>Użyte narzędzia</h2>
+  <ul class="tools">
+    <li>
+      <a href="https://wiki.nixos.org/wiki/Nix_(package_manager)">nix</a>
+      (menedżer pakietów), <a href="https://nixos.org/">NixOS</a> (system
+      operacyjny),
+      <a href="https://github.com/NixOS/nixpkgs/issues">nixpkgs</a> (kolekcja pakietów)
+    </li>
+    <li>
+      <a href="https://svelte.dev/">Svelte</a> oraz
+      <a href="https://svelte.dev/docs/kit/introduction">SvelteKit</a>
+    </li>
+    <li>hosting plików <a href="https://git.0x0.st/mia/0x0">0x0</a></li>
+    <li><a href="https://www.typescriptlang.org/">TypeScript</a></li>
+    <li><a href="https://sass-lang.com/documentation/syntax/">SCSS</a></li>
+  </ul>
+</div>
 
 <style lang="scss">
   ul.hearts {
@@ -45,10 +47,15 @@
   }
 
   h2 {
-    margin-left: 0.3rem;
     padding: 1rem;
     background: #dfe3eb;
     border-radius: 100pt;
     display: inline-block;
+  }
+
+  .content {
+    max-width: 64ch;
+    margin: 0 auto;
+    padding: 0 1rem;
   }
 </style>
