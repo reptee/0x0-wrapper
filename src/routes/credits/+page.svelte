adbf87ab2a26a7d9ed9bd423915f7590c03f5e10
Repetitive <git@crii.xyz>
2026-01-17 17:37:06 +0100

    polskie tłumaczenie /credits

--- src/routes/credits/+page.svelte
+++ src/routes/credits/+page.svelte
@@ -1,32 +1,30 @@
-<!-- TODO: hearts as list items prefix -->
-<h2>People</h2>
+<h2>Ludzie</h2>
 <ul class="hearts">
   <li>
-    <a href="https://git.0x0.st/mia">Mia</a> for creating 0x0 and hosting it for
-    everyone
+    <a href="https://git.0x0.st/mia">Mia</a> za stworzenie 0x0 i hostowanie go dla
+    wszystkich
   </li>
   <li>
-    Operators of
-    <a href="https://boop.icu">boop.icu</a> and
+    Operatorzy
+    <a href="https://boop.icu">boop.icu</a> oraz
     <a href="https://0.vern.cc">0.vern.cc</a>
   </li>
-  <li>Svelte team for the framework</li>
+  <li>Zespół Svelte za framework</li>
 </ul>
 
-<!-- TODO: hammer as list items prefix -->
-<h2>Tools used</h2>
+<h2>Użyte narzędzia</h2>
 <ul class="tools">
   <li>
-    <a href="https://wiki.nixos.org/wiki/Nix_(package_manager)">nix</a> the
-    package manager, <a href="https://nixos.org/">NixOS</a> the operating
-    system,
-    <a href="https://github.com/NixOS/nixpkgs/issues">nixpkgs</a> the package collection
+    <a href="https://wiki.nixos.org/wiki/Nix_(package_manager)">nix</a>
+    (menedżer pakietów), <a href="https://nixos.org/">NixOS</a> (system
+    operacyjny),
+    <a href="https://github.com/NixOS/nixpkgs/issues">nixpkgs</a> (kolekcja pakietów)
   </li>
   <li>
-    <a href="https://svelte.dev/">Svelte</a> and
+    <a href="https://svelte.dev/">Svelte</a> oraz
     <a href="https://svelte.dev/docs/kit/introduction">SvelteKit</a>
   </li>
-  <li><a href="https://git.0x0.st/mia/0x0">0x0</a> file hosting service</li>
+  <li>hosting plików <a href="https://git.0x0.st/mia/0x0">0x0</a></li>
   <li><a href="https://www.typescriptlang.org/">TypeScript</a></li>
   <li><a href="https://sass-lang.com/documentation/syntax/">SCSS</a></li>
 </ul>
